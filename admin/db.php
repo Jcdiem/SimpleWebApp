aa972ee547bb859a372c2243a2330d8e946ceeb4
Jcdiem <jcdiem@protonmail.com>
2021-03-12 18:25:34 -0600

    Commented MySQL Connection
    -Added a note to highlight the purposeful use of a backslash

--- admin/db.php
+++ admin/db.php
@@ -1,5 +1,6 @@
 
 <?php
+//                                       Password needed backslash to escape '$' character
 $mysqli = new mysqli("localhost", "webapadmin", "GD4s*4\$D@l*ANDP30@uG1%!t4TMe*x4^", "webapp");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
